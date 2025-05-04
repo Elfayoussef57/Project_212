90ab59dc778e3821fb680a9daf8ab795dacde9f4
Elfayoussef57 <pr.elfajriyoussef.57@gmail.com>
2025-05-04 17:41:15 +0000

    modified:   data_import.py

--- data_import.py
+++ data_import.py
@@ -1,7 +1,23 @@
 import tensorflow as tf
 import os
+import cv2
+import numpy as np
 
-print(os.listdir('../input'))
-print(tf.__version__)
-print(tf.config.list_physical_devices('GPU'))
-print(tf.config.list_physical_devices('CPU'))
+labels = ['PNEUMONIA','NORMAL']
+img_size = 128
+def get_data(data_dir):
+    data=[]
+    for label in labels:
+#         train/PNEUMONIA
+        path = os.path.join(data_dir, label)
+        class_num = labels.index(label)
+        for img in os.listdir(path):
+            try:
+                img_arr = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)
+                resized_arr = cv2.resize(img_arr, (img_size, img_size))
+                data.append([resized_arr, class_num])
+            except Exception as e:
+                print(e)
+    return np.array(data)
+
+
